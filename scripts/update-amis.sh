50a581cd4a5e896b8d9cb41b4d07eb14807347a7
Richard Hurt <rnhurt@gmail.com>
2017-05-12 12:00:12 -0400

    Update Linux AMI information

--- scripts/update-amis.sh
+++ scripts/update-amis.sh
@@ -13,8 +13,8 @@ for region in "${regions[@]}" ; do
   win2012_base=$(aws --region $region ec2 describe-images --owners amazon --filters "Name=platform,Values=windows" "Name=name,Values=Windows_Server-2012-R2_RTM-English-64Bit-Base-*" --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
   win2016_full=$(aws --region $region ec2 describe-images --owners amazon --filters "Name=platform,Values=windows" "Name=name,Values=Windows_Server-2016-English-Full-Base-*" --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
   win2016_nano=$(aws --region $region ec2 describe-images --owners amazon --filters "Name=platform,Values=windows" "Name=name,Values=Windows_Server-2016-English-Nano-Base-*" --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
-  linux_trusty=$(aws --region $region ec2 describe-images --owners 099720109477 --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*" --query 'Images[*]' --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
   linux_xenial=$(aws --region $region ec2 describe-images --owners 099720109477 --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*" --query 'Images[*]' --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
+  linux_yakkety=$(aws --region $region ec2 describe-images --owners 099720109477 --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-yakkety-16.10-amd64-server-*" --query 'Images[*]' --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
   linux_amazon=$(aws --region $region ec2 describe-images --owners amazon --filters "Name=name,Values=amzn-ami-hvm-2016*-gp2" --query 'Images[*]' --query 'Images[*].{Name:Name,ID:ImageId,Date:CreationDate}' --output text | sort | tail -n 1 | cut -f 2)
 
   # Wait for all the background processes to finish
@@ -22,13 +22,13 @@ for region in "${regions[@]}" ; do
 
   # Output pretty JSON ready to be copy & pasted into the globals.json file
   printf '{\n' > "${PWD}/blueprints/common/AMIs.${region}.json"
-  printf '  "win2012_core" : "%s",\n' $win2012_core >> "blueprints/common/AMIs.${region}.json"
-  printf '  "win2012_base" : "%s",\n' $win2012_base >> "blueprints/common/AMIs.${region}.json"
-  printf '  "win2016_full" : "%s",\n' $win2016_full >> "blueprints/common/AMIs.${region}.json"
-  printf '  "win2016_nano" : "%s",\n' $win2016_nano >> "blueprints/common/AMIs.${region}.json"
-  printf '  "linux_trusty" : "%s",\n' $linux_trusty >> "blueprints/common/AMIs.${region}.json"
-  printf '  "linux_ubuntu" : "%s",\n' $linux_xenial >> "blueprints/common/AMIs.${region}.json"
-  printf '  "linux_amazon" : "%s"\n' $linux_amazon >> "blueprints/common/AMIs.${region}.json"
+  printf '  "win2012_core": "%s",\n' $win2012_core  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "win2012_base": "%s",\n' $win2012_base  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "win2016_full": "%s",\n' $win2016_full  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "win2016_nano": "%s",\n' $win2016_nano  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "linux_amazon": "%s",\n' $linux_amazon  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "linux_ubults": "%s",\n' $linux_xenial  >> "blueprints/common/AMIs.${region}.json"
+  printf '  "linux_ubuntu": "%s"\n'  $linux_yakkety >> "blueprints/common/AMIs.${region}.json"
   printf '}\n' >> "${PWD}/blueprints/common/AMIs.${region}.json"
 
   echo 'done!'
