5dd194cc57a8607286eb3c6560f5e2ac67c796fd
Richard Hurt <rnhurt@gmail.com>
2017-06-20 12:42:52 -0400

    Add cfn-sync and upgrade AWS SDK module

---
+++ scripts/sync.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+
+var sync = require("../lib/sync");
+var cwd = process.cwd();
+
+sync(cwd, function(err) {
+  if (err) { console.error(err); return; }
+  console.log("Done.");
+});
