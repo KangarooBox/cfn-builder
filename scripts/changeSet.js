5aedcd41a2b13ff507f039bc84f53ab906ec4bad
Richard Hurt <rnhurt@gmail.com>
2019-10-26 07:10:51 -0400

    Update ChangeSet to class based

--- scripts/changeSet.js
+++ scripts/changeSet.js
@@ -4,14 +4,10 @@
 
 // Local
 var utilities = require("../lib/utilities");
-var changeSet = require('../lib/changeSet');
+var ChangeSet = require('../lib/changeSet');
 
 var args  = utilities.parseArguments();
 var cwd   = process.cwd();
 
-changeSet(cwd, args, function(err){
-  if (err) {
-    console.error(err);
-    return;
-  }
-});
+var changeSet = new ChangeSet(cwd, args);
+changeSet.create();
