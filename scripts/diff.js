4292a761851412da9efe24e2954413d1460d3e9c
Richard Hurt <richard.hurt@locushealth.com>
2020-02-08 17:31:47 -0500

    update diff to output table

--- scripts/diff.js
+++ scripts/diff.js
@@ -3,15 +3,48 @@
 'use strict';
 
 // Local
-var utilities = require("../lib/utilities");
-var diff      = require('../lib/diff');
+const utilities = require('../lib/utilities');
+const diff      = require('../lib/diff');
+const fs        = require('fs');
+const path      = require('path');
 
-var args  = utilities.parseArguments();
+var args  = utilities.parseArguments(true);
 var cwd   = process.cwd();
 
-diff(cwd, args.envName, args.projectName, function(err){
-  if (err) {
-    console.error(err);
-    return;
-  }
-});
+diff(cwd, args.envName, args.projectName)
+// .then(function(msg, delta){
+//   console.log(msg);
+// })
+
+// blah.then(function(msg, delta){
+//       console.log(msg);
+//     })
+//     .catch(function(err){
+//       console.error(err);
+//     });
+
+// (async (dirPath) => {
+//   const dir = await fs.promises.opendir(dirPath);
+//   for await (const file of dir) {
+
+//     var filePath = path.join(dirPath, file.name);
+
+//     fs.stat(filePath, function(err, stat){
+//       if (!stat.isDirectory()) {
+//         var nameArr = file.name.split('.');
+
+//         diff(cwd, nameArr[1], nameArr[0], function(err, msg, delta){
+//           if(err){
+//             // console.error(err);
+//           } else {
+//             if(delta){
+//               console.log(`${nameArr[0]}.${nameArr[1]} is different!`);
+//             }
+//             // console.log(delta);
+//             // console.log(msg);
+//           }
+//         })
+//       }
+//     });
+//   }
+// })('./blueprints/stacks');
