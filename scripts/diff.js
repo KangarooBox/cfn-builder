3b9923f61cad20535b673e1cb17c01b384ce882a
Richard Hurt <richard.hurt@locushealth.com>
2020-02-11 14:58:00 -0500

    update diff to show single blueprint changes

--- scripts/diff.js
+++ scripts/diff.js
@@ -12,39 +12,3 @@ var args  = utilities.parseArguments(true);
 var cwd   = process.cwd();
 
 diff(cwd, args.envName, args.projectName)
-// .then(function(msg, delta){
-//   console.log(msg);
-// })
-
-// blah.then(function(msg, delta){
-//       console.log(msg);
-//     })
-//     .catch(function(err){
-//       console.error(err);
-//     });
-
-// (async (dirPath) => {
-//   const dir = await fs.promises.opendir(dirPath);
-//   for await (const file of dir) {
-
-//     var filePath = path.join(dirPath, file.name);
-
-//     fs.stat(filePath, function(err, stat){
-//       if (!stat.isDirectory()) {
-//         var nameArr = file.name.split('.');
-
-//         diff(cwd, nameArr[1], nameArr[0], function(err, msg, delta){
-//           if(err){
-//             // console.error(err);
-//           } else {
-//             if(delta){
-//               console.log(`${nameArr[0]}.${nameArr[1]} is different!`);
-//             }
-//             // console.log(delta);
-//             // console.log(msg);
-//           }
-//         })
-//       }
-//     });
-//   }
-// })('./blueprints/stacks');
